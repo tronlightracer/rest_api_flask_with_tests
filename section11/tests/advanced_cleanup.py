2276ce3a61c6c48cadf3072dba128d3af46fbac7
tronlightracer <treyvontron225@gmail.com>
2022-08-12 13:13:13 -0600

    updating the test so it adds a post if no store is found

--- section11/tests/advanced_cleanup.py
+++ section11/tests/advanced_cleanup.py
@@ -1,11 +1,12 @@
-import json
 import requests
 from pprint import pprint
 import argparse
 
 store_name_parser = argparse.ArgumentParser()
-store_name_arg = store_name_parser.add_argument("--name")
-store_name = store_name_parser.parse_args().name
+store_name_arg = store_name_parser.add_argument("--store_name")
+item_name_arg = store_name_parser.add_argument("--item_name")
+store_name = store_name_parser.parse_args().store_name
+item_name = store_name_parser.parse_args().item_name
 
 my_app = "http://127.0.0.1:3200/"
 r_bin = "https://httpbin.org/"
@@ -55,66 +56,9 @@ def test_cleanup():
 
 test = test_cleanup()
 
-# def advance_cleanup(name):
 
-#     got = requests.get(
-#     my_app + "stores"
-#         )
-#     print("got", got)
-#     print(got.json()['stores'])
-#     if got.json()['stores'] == None:
-#         print("the list is empty")
-#     store_dict = got.json()
-#     for i in store_dict['stores']:
-#         for v in i.values():
-#             if v == name:
-#                 deleted = requests.delete(
-#                     my_app + f"store/{name}",
-#                     headers=header2,
-#                 )
-#                 print("deleted", deleted)
-#                 return deleted.json()
-                
-#             else:
-#                 posted = requests.post(
-#                     my_app + f"store/{name}",
-#                     headers=header2,
-#                 )
-#                 print("posted", posted)
-#                 return posted.json()
-    
 
-# cleanup = advance_cleanup(store_name)
-
-def advance_cleanup(name):
-
-    got = requests.get(
-    my_app + "stores"
-        )
-    print("got", got)
-    #print(got.json()['stores'])
-    if not got.json()['stores']:
-        posted = requests.post(
-            my_app + f"store/{name}",
-            headers=header2,
-        )
-        print("posted json:", posted.json())
-        return posted
-    else:
-        for i in got.json()['stores']:
-           for v in i.values():
-            if v == name:
-                deleted = requests.delete(
-                    my_app + f"store/{name}",
-                    headers=header2,
-                ) 
-                print("deleted json:", deleted.json())
-                return deleted
-    
-
-# cleanup = advance_cleanup(store_name)
-
-def advance_cleanup2(name):
+def advance_cleanup(name, item):
     
     access_token = login.json()['access_token']
     header2 = {"Content-Type": "application/json", "Authorization": "Bearer " + access_token}
@@ -125,52 +69,45 @@ def advance_cleanup2(name):
     print("got", got)
     print(got.json())
 
-    if not got.json()['stores']:
+    get_items = requests.get(
+        my_app + "items"
+    )
+    print(get_items)
+
+    if not got.json()['stores']: #and not get_items.json():
         posted = requests.post(
             f"{my_app}/store/{name}",
             headers=header2,
         )
         print(f"post json data: {posted.json()}")
-        return posted
+        assert posted.json() == {'id': 1, 'name': store_name, 'items': []}
+        
+        item_post = requests.post(
+            f"{my_app}/item/{item}",
+            headers=header2,
+            json={"username": usern, "password": passw, "price": 22, "store_id": 1}
+        )
+        print(f"post item json data: {item_post.json()}")
+        item1 = {'id': 1, 'name': item, 'price': 22.0, 'store_id': 1}
+        assert item_post.json() == item1
+
     else:
-        for store in got.json()['stores']:
-            for v in store.values():
-                if v == name:
-                    deleted = requests.delete(
-                        f"{my_app}/store/{name}",
-                        headers=header2,
-                    )
-                    print(f"deleted json: {deleted.json()}")
-                    return deleted
-                else:
-                    deleted = requests.delete(
-                        f"{my_app}/store/{name}",
-                        headers=header2,
-                    )
-                    print(f"deleted json: {deleted.json()}")
-                    return deleted
+        for v in got.json()['stores'][0].values():
+            if v == name:
+                deleted = requests.delete(
+                    f"{my_app}/store/{name}",
+                    headers=header2,
+                )
+                print(f"deleted json: {deleted.json()}")
+                assert deleted.json() == {'message': 'Store deleted'}
+                return deleted
+            else:
+                posted2 = requests.post(
+                    f"{my_app}/store/{name}",
+                    headers=header2,
+                )
+                print(f"posted json: {posted2.json()}")
+                assert {'id': 1, 'name': 'store1', 'items': [{'id': 1, 'name': item, 'price': 22.0, 'store_id': 1}]}
+                return posted2
 
-
-
-cleanup = advance_cleanup2(store_name)
-
-# get_stores = requests.get(
-#     my_app + "stores"
-# )
-# print(get_stores.json())
-
-
-# store_post = requests.post(
-#     my_app + f"store/{store_name}",
-#     #json={"stores": ['store1']},
-#     headers=header2,
-# )
-
-# store_del = requests.post(
-#     my_app + f"store{store_name}",
-#     headers=header2,
-# )
-
-# print(store_post)
-# print(store_del)
-# pprint(store_post.json())
+print(advance_cleanup(store_name, item_name))
